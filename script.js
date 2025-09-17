d56c7e35396cfe814f0cc45fe5cca327fb9b8be5
Emiliano Muñoz <emilianom01@gmail.com>
2025-09-17 14:09:07 -0600

    Laboratorio HTML, CSS, JS

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("red").textContent = "Adiós";
+
+document.querySelector(".blue").style.color = "orange"; 
+
+document.getElementById("clic").addEventListener("clic", function() {
+    this.style.color = "brown";
+});
